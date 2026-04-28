1a801cb094af28b2cbcb47bae160677e724ba2b5
Simon6764 <s.b.wessel@student.tue.nl>
2026-04-28 11:29:41 +0200

    testfunction

---
+++ tests/test_graph_processing.py
@@ -0,0 +1,11 @@
+from graph_processing import GraphProcessor
+
+gp = GraphProcessor(
+    vertex_ids=[0, 1, 2],
+    edge_ids=[10, 20],
+    edge_vertex_id_pairs=[(0, 1), (1, 2)],
+    edge_enabled=[True, True],
+    source_vertex_id=0
+)
+
+print("Graph created successfully!")
